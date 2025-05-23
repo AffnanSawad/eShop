62d9f9d6a33cca9b0bd05f7bcdf52dcc52afebf6
Affnan Sawad <affnansawad2002@gmail.com>
2025-05-23 21:18:50 +0600

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+const repoName = 'eShop-FrontEnd'
+
 // https://vite.dev/config/
 export default defineConfig({
+
+  base: `/${repoName}/`,
+
   plugins: [react() , tailwindcss(), ],
 })
